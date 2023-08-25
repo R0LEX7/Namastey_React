53f4df9116ccd781843ab008d39ae8ff60028263
R0LEX& <himanshugola1111@gmail.com>
2023-08-25 21:59:56 +0530

    Day 03 Laying the foundation

--- Namastey_React/EP2/app.js
+++ Namastey_React/EP2/app.js
@@ -1,11 +1,14 @@
-import React from 'react';
-import Fun from './Fun';
+import React from "react";
+import Fun from "./Fun";
 
 export function App() {
-    return (
-        <div>
-            <Fun/>
-            <h1>Hello React!</h1>
-        </div>
-    );
+  const title = () =>  <h1>this is my title</h1>;
+  console.log("hyy");
+  return (
+    <div>
+      <Fun />
+        {title()}
+      <h1>Hello React!</h1>
+    </div>
+  );
 }
