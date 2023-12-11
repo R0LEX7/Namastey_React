fd9f5bc3d11d72019a39037c0f52e3c417129237
Himanshu Kumar Gola <himanshugola1111@gmail.com>
2023-12-11 16:32:48 +0530

    Revert "Initial commit"
    This reverts commit b480a1341397d256b5ced9d6b1bca78100a8767c.

--- programs/Projects/Pintrest clone/frontend/src/index.js
+++
@@ -1,8 +0,0 @@
-import Home from "./pages/Home";
-import Header from "./components/Header";
-import Login from "./components/Login";
-import Register from "./components/Register";
-import Feed from "./components/Feed";
-import Profile from "./components/Profile";
-import PostForm from "./components/PostForm";
-export { Home, Header, Login, Register, Feed , Profile , PostForm};
